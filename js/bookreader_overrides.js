2d39a317ee99d1cf65ec434242db9a4683e801b0
root <root@vassardev.i7.dgicloud.com>
2013-06-06 12:30:56 +0000

    Updated OCR/text to preserve white space

--- js/bookreader_overrides.js
+++ js/bookreader_overrides.js
@@ -7,6 +7,8 @@
   IslandoraBookReader.prototype.buildFullTextDiv = function(jFullTextDiv) {
     jFullTextDiv.find('.BRfloatMeta').height(600);
     jFullTextDiv.find('.BRfloatMeta').width(600);
+    console.log("In override function");
+    console.log("mode: " + this.mode);
     if (3 == this.mode) {
       jFullTextDiv.find('.BRfloatMeta').html('<div>' + Drupal.t('Full text not supported for this view.') + '</div>');
     }
@@ -16,7 +18,11 @@
         pids: []
       };
       if (this.mode == 1) {
-        request_data.pids.push(this.getPID(index));
+        var hash_arr = this.oldLocationHash.split("/");
+        var index = hash_arr[1];
+        var pid = this.getPID(index-1);
+        console.log("pid: " + pid);
+        request_data.pids.push(pid);
       }
       else if (this.mode == 2) {
         var indices = this.getSpreadIndices(index);
@@ -26,7 +32,8 @@
       }
       $.get(Drupal.settings.vassardora.text_url, request_data,
         function(data) {
-          jFullTextDiv.find('.BRfloatMeta').html(data);
+          console.log(data);
+          jFullTextDiv.find('.BRfloatMeta').html("<pre>" . data . "</pre>");
 
           // Show the related element, when clicked.
           $('li.vassardora_fulltext_link').click(function(eventObject) {
